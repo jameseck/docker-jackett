3500489a1309cd64eff35daad59cf71da5bfc08c
James Eckersall <james.eckersall@gmail.com>
2019-06-01 17:24:46 +0100

    added check option to build script

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+if [ "$1" == "--check" ] || [ "$1" == "-c" ]; then
+  CHECKONLY=true
+fi
+
 # Latest release
 URL=$(curl -s https://api.github.com/repos/Jackett/Jackett/releases | jq -r "[ .[] | .assets[] | select(.name | test(\"Jackett.Binaries.Mono.tar.gz\")) ] | first | .browser_download_url")
 
@@ -10,6 +14,13 @@ git pull > /dev/null 2>&1
 DOCKERFILE_VERSION=$(grep "^ARG JACKETT_VERSION=" Dockerfile | cut -f2 -d\=)
 
 if [ "${VERSION}" != "${DOCKERFILE_VERSION}" ]; then
+  if [ "${CHECKONLY}" == "true" ]; then
+    echo "There is a new version available"
+    echo "Current version: ${DOCKERFILE_VERSION}"
+    echo "Available version: ${VERSION}"
+    exit 0
+  fi
+
   echo "Updating Dockerfile with version ${VERSION}"
   sed -i -e "s/^\(ARG JACKETT_VERSION=\).*$/\1${VERSION}/g" \
          -e "s|^\(ARG JACKETT_URL=\).*$|\1${URL}|g" Dockerfile
